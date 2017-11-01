507670e1ef7ecbc81320cb49bf9687e095a52deb
Nazrul <nazrulku07@gmail.com>
2017-11-01 22:40:57 +0600

    seed for department and zone

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
