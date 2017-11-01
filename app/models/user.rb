4d5b984b481fb0c0cb72a5642eb76fca62fc983f
Nazrul <nazrulku07@gmail.com>
2017-11-01 22:04:02 +0600

    add basic layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  ROLES = ['It Admin', 'Power User', 'Admin', 'Technician']
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :role
+
+  def it_admin
+    role.name.downcase == 'it admin'
+  end
+
+  def power_user
+    role.name.downcase == 'power user'
+  end
+
+  def admin
+    role.name.downcase == 'admin'
+  end
 end
